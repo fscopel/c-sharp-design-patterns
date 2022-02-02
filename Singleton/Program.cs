9e8ab942d9c2fc0ea4793afad75f0e406189942e
Fabio Scopel <fabioscopel99@gmail.com>
2022-02-01 19:00:04 -0800

    Singleton pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,10 @@
+﻿using Singleton;
+
+var one = ThereCanBeOnlyOne.Instance;
+var two = ThereCanBeOnlyOne.Instance;
+one.Value++;
+Console.WriteLine($"{two.Value}");
+two.Value++;
+Console.WriteLine($"{one.Value}");
+
+Console.ReadLine();
---
+++ Singleton/ThereCanBeOnlyOne.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal sealed class ThereCanBeOnlyOne
+    {
+        private static volatile ThereCanBeOnlyOne? _instance;
+        private static readonly object _lock = new object();
+
+        private ThereCanBeOnlyOne() { }
+
+        public static ThereCanBeOnlyOne Instance
+        {
+            get
+            {
+                if (_instance != null)
+                {
+                    return _instance;
+                }
+
+                lock (_lock)
+                {
+                    if (_instance == null)
+                        _instance = new ThereCanBeOnlyOne();
+                }
+
+                return _instance;
+
+            }
+        }
+
+        public int Value { get; set; }
+
+
+    }
+}
